cf2114c97f4efc22f0a7b1fd73b23877cdf40aac
lonewolf2580 <linusngwee@yahoo.com>
2021-10-15 16:04:14 -0700

    Delete Todo - 2 files modified

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import Todos from './Todos';
 export default class App extends Component {
   state = {
     todos: [
-      {id: 1, content: 'buy some milk'},
-      {id: 2, content: 'play mario kart'}
-    ]
-  }
-  render(){
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario kart' },
+    ],
+  };
+  deleteTodo = (id) => {
+    // alert(id);
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  };
+  render() {
     return (
       <div className="App container">
         <h1 class="center blue-text">Todos</h1>
-         <Todos todos={this.state.todos} />   
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
